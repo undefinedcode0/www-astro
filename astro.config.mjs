70453e3131ce3c9858a2434c26c271a6c82d56d9
undefinedcode0 <anoncyn@proton.me>
2025-10-21 17:19:16 -0500

    comment out partytown in astro.config.mjs and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
+// import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 
 import rehypeAnimationDelay from './plugins/rehypeAnimationDelay.js';
