e87a16817f87440f4e381007dab2574e186bd1c6
undefinedcode0 <anoncyn@proton.me>
2026-01-12 20:58:16 -0600

    IM SO FUCKING SCARED

--- src/OgRender.tsx
+++ src/OgRender.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const { twj } = await import('tw-to-css');
 
 export function fnRender({ title, description }: RenderFunctionInput): Promise<React.ReactNode> {
+  const stripes = [1, 2, 4, 8, 16, 32, 64, 128, 256];
   return Promise.resolve(
     <div style={twj('flex flex-col w-full h-full justify-start flex-col bg-[#F7F5ED]')}>
       <div style={twj('flex justify-start items-start flex-col p-20')}>
@@ -12,15 +13,9 @@ export function fnRender({ title, description }: RenderFunctionInput): Promise<R
         </div>
         <p style={twj('text-[50px] text-black text-left')}>{description}</p>
       </div>
-      <div style={twj('h-[1px] mb-[1px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[2px] mb-[2px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[4px] mb-[4px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[8px] mb-[8px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[16px] mb-[16px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[32px] mb-[32px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[64px] mb-[64px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[128px] mb-[128px] bg-[#125EFF] w-full')}></div>
-      <div style={twj('h-[256px] mb-[256px] bg-[#125EFF] w-full')}></div>
+      {stripes.map((h) => (
+        <div key={h} style={twj(`h-[${h}px] mb-[${h}px] bg-[#125EFF] w-full`)}></div>
+      ))}
     </div>,
   );
 }
